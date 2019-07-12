e3142a1ec125a72bb8b6ca07182e0501e0ced23f
memoshasinha <39963915+memoshasinha@users.noreply.github.com>
2019-07-12 20:12:45 +0530

    Update bracket_validator.test.js
    Added valid and invalid test cases

--- bracket_validator/bracket_validator.test.js
+++ bracket_validator/bracket_validator.test.js
@@ -6,3 +6,14 @@ test('input is empty', () => {
     expect(validateExpression(input)).toStrictEqual(error);
 });
 
+test('input is valid', () => {
+    let input = "{if(2>1{console.log('valid');})}";
+    expect(validateExpression(input)).toStrictEqual("Valid expression");
+});
+
+test('input is invalid', () => {
+    let input = "{if(2>1{console.log('valid');)}";
+    expect(validateExpression(input)).toStrictEqual("Invalid expression");
+});
+
+
